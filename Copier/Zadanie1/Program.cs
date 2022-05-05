01558060e5f1c0053b5da37e8ee1674ddf67627e
iambartek <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-05-05 17:19:41 +0200

    Revert "ScanAndPrint"
    This reverts commit 81c5f5948a3fa0f12536bedbbe01c98394a75e70.

--- Copier/Zadanie1/Program.cs
+++ Copier/Zadanie1/Program.cs
@@ -1,16 +1,2 @@
-﻿
-using ver1;
-using Zadanie1;
-
-var xerox = new Copier();
-xerox.PowerOn();
-IDocument doc1 = new PDFDocument("aaa.pdf");
-xerox.Print(in doc1);
-
-IDocument doc2;
-xerox.Scan(out doc2);
-
-xerox.ScanAndPrint();
-Console.WriteLine(xerox.Counter);
-Console.WriteLine(xerox.PrintCounter);
-Console.WriteLine(xerox.ScanCounter);
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
