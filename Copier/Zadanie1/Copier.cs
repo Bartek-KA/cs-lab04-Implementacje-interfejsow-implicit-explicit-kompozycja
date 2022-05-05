e73a12e5d5bba250235321ab6e6fd525fd09e7e4
iambartek <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-05-05 15:55:59 +0200

    Print

---
+++ Copier/Zadanie1/Copier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ver1;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; private set; } = 0;
+        public int ScanCounter { get; private set; } = 0;
+
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.on)
+            {
+                Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+                PrintCounter++;
+            }
+            else return;
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
